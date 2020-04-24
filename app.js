5947475db57e6fb6e0df6f069477434407b64263
Quentin79140 <60688891+Quentin79140@users.noreply.github.com>
2020-04-24 16:56:27 +0200

    init

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 const handlebars = require("handlebars");
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars")
+
+
+
+const app = express()
+
+//Mongoose 
+mongoose.connect('mongodb://localhost:27017/actu-asp')
+
+//bodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+//route
+app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs"}));
+app.set('view engine', 'hbs');
+
+
+// route 
+
+app.get(("/"), (req, res) => {
+    res.render("home");
+});
+
+
+
+
+
+
+
+app.listen(4000, function(){
+    console.log("écoute sur le port 4000");
+    
+})
